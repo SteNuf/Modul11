bf1f0722a258a3fa7359d65ef691255fa1cdea32
SteNuf <stephan.nufer@gmx.de>
2025-12-06 16:14:46 +0100

    Create the sections TimeLeft and TimeInput.

--- Kapitel 3 Aufgabe 1/timer-task/src/App.tsx
+++ Kapitel 3 Aufgabe 1/timer-task/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ButtonArea from "./assets/section/components/button_area/ButtonArea";
 import Timeinput from "./assets/section/components/time_input/Timeinput";
+import TimeLeft from "./assets/section/components/time_left/TimeLeft";
 
 function App() {
   return (
     <div className="App">
       <Timeinput />
+      <TimeLeft />
       <ButtonArea />
     </div>
   );
---
+++ Kapitel 3 Aufgabe 1/timer-task/src/assets/section/components/time_left/TimeLeft.tsx
@@ -0,0 +1,10 @@
+function TimeLeft() {
+  return (
+    <div>
+      <h1 className="time-left__title">Time left:</h1>
+      <span className="time-left__seconds">0.000s</span>
+    </div>
+  );
+}
+
+export default TimeLeft;
