6f90196964b43c5888b64a94e7728d4395fef4ff
SteNuf <stephan.nufer@gmx.de>
2025-12-07 18:23:28 +0100

    finish the task

--- Kapitel 3 Aufgabe 1/timer-task/src/App.tsx
+++ Kapitel 3 Aufgabe 1/timer-task/src/App.tsx
@@ -1,14 +1,87 @@
 import "./App.css";
-import ButtonArea from "./assets/section/components/button_area/ButtonArea";
-import Timeinput from "./assets/section/components/time_input/Timeinput";
-import TimeLeft from "./assets/section/components/time_left/TimeLeft";
+import { useEffect, useRef, useState } from "react";
+
+function useFormInput() {
+  const [value, setValue] = useState<number>(0);
+  function handleInputChangeEvent(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(Number(event.target.value));
+  }
+
+  return { value, handleInputChangeEvent };
+}
 
 function App() {
+  const { value, handleInputChangeEvent } = useFormInput();
+  const [startTime, setStartTime] = useState(0);
+  const [now, setNow] = useState(0);
+  const [pause, setPause] = useState(false);
+  const intervalId = useRef(-1);
+
+  useEffect(() => {
+    if ((now - startTime) / 1000 >= value) {
+      clearInterval(intervalId.current);
+      setNow(startTime + value * 1000);
+    }
+  }, [startTime, now, value]);
+
+  function startInterval() {
+    intervalId.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStart() {
+    if (!pause) {
+      clearInterval(intervalId.current);
+      setPause(false);
+      const dateNow = Date.now();
+      setStartTime(dateNow);
+      setNow(dateNow);
+
+      startInterval();
+    } else {
+      const elapsepdTime = now - startTime;
+      const dateNow = Date.now();
+      setNow(dateNow);
+      setStartTime(dateNow - elapsepdTime);
+      setPause(false);
+      startInterval();
+    }
+  }
+
+  function handlePause() {
+    clearInterval(intervalId.current);
+    setPause(true);
+  }
+
+  function handleReset() {
+    clearInterval(intervalId.current);
+    setPause(false);
+    setStartTime(0);
+    setNow(0);
+  }
+
+  const secondsLeft = value - (now - startTime) / 1000;
+
   return (
-    <div className="App">
-      <Timeinput />
-      <TimeLeft />
-      <ButtonArea />
+    <div>
+      <span className="t-4">Zeit testlegen:</span>
+      <br />
+      <input
+        type="number"
+        className="input"
+        value={value}
+        onChange={handleInputChangeEvent}
+      />
+      <br />
+      <span className="t-1 bold">Time left: </span>
+      <br />
+      <span className="t-1 bold">{secondsLeft.toFixed(3)}</span>
+      <div className="button-row">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
--- Kapitel 3 Aufgabe 1/timer-task/src/Component/input/Input.tsx
+++
@@ -1,4 +0,0 @@
-function Input() {
-  return <input type="text" placeholder="Enter time in seconds" />;
-}
-export default Input;
